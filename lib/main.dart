0fc89d9c75822d6c7dc66b54a785cbcd1dc3fb77
Usman Ghani <usmanghani.web@gmail.com>
2025-01-30 16:13:57 +0500

    Update 1
    Updated the code completely build the code

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,68 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(
+    int noteNum,
+  ) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$noteNum.wav'));
+  }
+
+  Expanded buildKey({required Color color, required int noteNum}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.zero),
+          ),
+        ),
+        onPressed: () {
+          playSound(noteNum);
+        },
+        child: Text(' '),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource('assets/note1.wav'));
-              },
-              child: Text('Click Here'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(
+                color: Colors.red,
+                noteNum: 1,
+              ),
+              buildKey(
+                color: Colors.orange,
+                noteNum: 2,
+              ),
+              buildKey(
+                color: Colors.yellow,
+                noteNum: 3,
+              ),
+              buildKey(
+                color: Colors.green,
+                noteNum: 4,
+              ),
+              buildKey(
+                color: Colors.teal,
+                noteNum: 5,
+              ),
+              buildKey(
+                color: Colors.blue,
+                noteNum: 6,
+              ),
+              buildKey(
+                color: Colors.purple,
+                noteNum: 7,
+              ),
+            ],
           ),
         ),
       ),
