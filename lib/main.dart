43708f9f82e7f3923769b91e4a7f753ea45d12aa
Usman Ghani <usmanghani.web@gmail.com>
2025-01-30 13:20:45 +0500

    New Repository

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+// import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () async {
+                final player = AudioPlayer();
+                await player.play(AssetSource('assets/note1.wav'));
+              },
+              child: Text('Click Here'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
